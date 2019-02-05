fd241dfaffd7a010ec279e1a8da9f47de94a6aa4
Jaime Solsona Alvarez <jsolsona@gamma28.aulas.gsyc.urjc.es>
2019-02-05 12:45:04 +0100

    meto tablas de multiplicar

---
+++ tablas_mult.py
@@ -0,0 +1,5 @@
+for num1 in range(1,11):
+    print("Tabla de multiplicar del " + str(num1))
+    print("-----------")
+    for num2 in range(1,11):
+        print(str(num1) + " por " + str(num2) + " es " + str(num1*num2))
